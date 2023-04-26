6cb364b8555d7eeb368192c2ab5a7de8c085a5a3
atxr <alexandre.tullot@protonmail.com>
2023-04-27 01:11:18 +0200

    Work (but fail) on brachio

--- rev-brachiosaure/test.py
+++ rev-brachiosaure/test.py
@@ -1,17 +1,34 @@
 from z3 import *
+import os
 import itertools
 import math
 from hashlib import sha512
 from sys import argv
 import qrcode
-from PIL import Image
+from PIL import Image, ImageOps
 from pyzbar import pyzbar
 import numpy as np
+import time
+
+
+def get_concat_h(im1, im2):
+    dst = Image.new("RGB", (im1.width + im2.width, im1.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (im1.width, 0))
+    return dst
+
+
+def get_concat_v(im1, im2):
+    dst = Image.new("RGB", (im1.width, im1.height + im2.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (0, im1.height))
+    return dst
+
 
 def decode_xy():
-    imgx = Image.open('x.png')
-    imgxinv = Image.open('xinv.png')
-    imgy = Image.open('y.png')
+    imgx = Image.open("x.png")
+    imgxinv = Image.open("xinv.png")
+    imgy = Image.open("y.png")
 
     rdx = pyzbar.decode(imgx)[0].data
     rdxinv = pyzbar.decode(imgxinv)[0].data
@@ -21,10 +38,10 @@ def decode_xy():
         d = []
         c = -1
         for i in range(len(rd)):
-            if rd[i] & 0xc0 == 0xc0:
-                c = (rd[i] << 6) & 0xff
+            if rd[i] & 0xC0 == 0xC0:
+                c = (rd[i] << 6) & 0xFF
             elif c != -1:
-                d.append(rd[i] & 0x3f ^ c)
+                d.append(rd[i] & 0x3F ^ c)
                 c = -1
             else:
                 d.append(rd[i])
@@ -38,36 +55,28 @@ def decode_xy():
     print(dx)
     print(dy)
 
-def cross(l1, l2, i, j, s):
+
+def sub_mul(l1, l2, i, j, s):
     r = 0
     for k in range(s):
-        r += l1[s*i + k]*l2[j + s*k]
-        r &= 0xff
+        r += l1[s * i + k] * l2[j + s * k]
+        r &= 0xFF
     return r
 
-def f(l1, l2, s, v=False):
-    B = True
-    X2 = [-1] *(s**2) 
-    for i in range(s):
-        if i % 10 == 0:
-            print(f"{i}/{s}")
 
-        for j in range(s):
-            r = cross(l1, l2, i, j, s)
-            X2[i*8+j] = r
-            if i == j:
-                B = r == 1 and B
-                if v and r != 1:
-                    print(f"{i=} {j=} {r=}")
-            else:
-                B = r == 0 and B
-                if v and r != 0:
-                    print(f"{i=} {j=} {r=}")
+def mul(l1, l2, n):
+    r = [-1] * (n**2)
+    for i in range(n):
+        for j in range(n):
+            s = 0
+            for k in range(n):
+                if l1[i * n + k] == 1 and l2[k * n + j] == 1:
+                    s = (s + 1) & 0xFF
 
-        if v and B == 0:
-            print(f"{i=} {j=}")
+            r[i * 8 + j] = s
 
-    return X2, B
+    return r
+
 
 def sol(a1, a2, n, t=5):
     s = Solver()
@@ -75,92 +84,90 @@ def sol(a1, a2, n, t=5):
 
     def avg(l, i, j):
         r = 0
-        for k1 in range(i, i+t):
-            for k2 in range(j, j+t):
-                if l[k1*n + k2]:
+        for k1 in range(i, i + t):
+            for k2 in range(j, j + t):
+                if l[k1 * n + k2]:
                     r += 1
         return r > 0.5
 
-    correctness = [avg(a1, i, j) == X[i][j] for i in range(n//t) for j in range(n//t)]
-    constr = f(a1, a2, n)
+    correctness = [
+        avg(a1, i, j) == X[i][j] for i in range(n // t) for j in range(n // t)
+    ]
+    constr = mul(a1, a2, n)
 
     s.add(constr)
     s.add(correctness)
+
 
 def main():
     if len(argv) != 2:
         print("Need username")
         exit(1)
 
+    t0 = time.time()
+
     hash = sha512(argv[1].encode()).digest()
+    hl = np.array(list(hash), dtype=c_uint8)
+    hl = hl.reshape(8, 8)
+    y = hl @ hl
+    yv = y.reshape(1, 64)[0]
 
+    ix = qrcode.make(bytes(hash), box_size=2, border=1).get_image()
+    iy = qrcode.make(bytes(y), box_size=2, border=1).get_image()
+    n = ix.width
+    N = n * n
+
+    ix.save("x.png")
+    iy.save("y.png")
+
+    ax = np.asmatrix(ix, "L")
+    ay = np.copy(np.asmatrix(iy, "L"))
+    lx = ax.reshape(1, n * n).tolist()[0]
+    ly = ay.reshape(1, n * n).tolist()[0]
+
+    m = 200
     s = Solver()
-    X = [BitVec(f"x{i}", 8) for i in range(64)]
-    Y = [BitVec(f"y{i}", 8) for i in range(64)]
-    
-    X2, _ = f(X, X, 8)
+    X = [Int(f"x{i}_{j}") for j in range(n + m) for i in range(n + m)]
+    Y = [Int(f"y{i}_{j}") for j in range(n + m) for i in range(n + m)]
 
-    data_user_c = [hash[i] == X[i] for i in range(64)]
-    data_serial_c = [X2[i] == Y[i] for i in range(64)]
+    s.add(
+        [
+            And(X[i * n + j] == lx[i * n + j], Y[i * n + j] == ly[i * n + j])
+            for i in range(n)
+            for j in range(n)
+        ]
+    )
+    s.add(
+        [Or(X[i * n + j] == 1, X[i * n + j] == 0) for j in range(n) for i in range(n)]
+    )
+    s.add(
+        [Or(Y[i * n + j] == 1, Y[i * n + j] == 0) for j in range(n) for i in range(n)]
+    )
 
-    chall_c = data_user_c 
-    chall_c += data_serial_c
+    Z = mul(X, Y, n + m)
+    s.add(
+        [
+            Z[i * n + j] == (1 if i == j else 0)
+            for i in range(n + m)
+            for j in range(n + m)
+        ]
+    )
 
-    s.add(chall_c)
+    print(s.check())
     if s.check() == sat:
         m = s.model()
-        x = [m.evaluate(X[i]).as_long() for i in range(64)]
-        y = [m.evaluate(Y[i]).as_long() for i in range(64)]
+        x = np.array([X[i * n + j].evaluate() for j in range(n) for i in range(n)])
+        y = np.array([Y[i * n + j].evaluate() for j in range(n) for i in range(n)])
 
-        border = 60
-        imgx = qrcode.make(bytes(x), box_size=9, border=0)
-        imgy = qrcode.make(bytes(y), box_size=9, border=0)
-
-        arrx = np.array(imgx)
-        arry = np.array(imgy)
-        farrx = [x for l in arrx for x in l]
-        farry = [y for l in arry for y in l]
-        n = imgx.get_image().width
-
-        #sol(farrx, farry, n)
-        mx = np.asmatrix(arrx)
-        mxinv = np.invert(mx)
-        mx2 = mx * mx
-
-        Image.fromarray(mxinv).save("xinv.png")
-        Image.fromarray(mxinv).save("x2.png")
-
-        """
-        over = cross(farrx, farry, 0, 0, n) - 1
-        print(over)
-        c = 0
-        while math.comb(c, over) < border:
-            c += 1 
-        
-        combs = itertools.combinations(range(c), over)
-        for i in range(border):
-            js = next(combs)
-            for j in js:
-                arrx[i,j] = False
-
-            
-        farrx = [x for l in arrx for x in l]
-        farry = [y for l in arry for y in l]
-        """
-
-        _, B = f(farrx, farry, n)
-        arrx[0][0] = 1
-        Image.fromarray(arrx).save("x.png")
-        Image.fromarray(arry).save("y.png")
-        if B:
-            print("WAOUW")
-
-        else:
-            print("sorry")
+        Image.fromarray(x).save("x.png")
+        Image.fromarray(y).save("y.png")
 
     else:
         print("unsat")
-        exit(1)
+
+    print("Time:")
+    print(str((time.time() - t0) / 60) + "min")
+
 
 main()
-decode_xy()
+# decode_xy()
